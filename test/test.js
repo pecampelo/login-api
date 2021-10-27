5580adbc176cb6ab2565231d794e2c2b40ed9c49
Pedro Campelo Matheus <pedrohcmatheus@gmail.com>
2021-10-27 17:00:36 -0300

    added tests until /signup

--- test/test.js
+++ test/test.js
@@ -1,17 +1,70 @@
 const assert = require('assert');
+const http = require('http');
 const { config } = require('../dist/config');
-const { default: server} = require('../dist/src/server');
+const { default: server } = require('../dist/src/server');
 
 server.listen(config);
 
-address = (`http://${config.host}:${config.port}`)
+const address = (`http://${config.host}:${config.port}`)
+const intendedRoute = `http://${config.host}:${config.port}`;
 
 
 describe("API", () => {
-  const intendedRoute = `http://${config.host}:${config.port}`;
 
   it(`should listen to server on ${intendedRoute}`, () => {
     assert.equal(address, intendedRoute);
   });
 
+  it(`should output method and endpoint to the console when receiving requests`, () => {
+
+  });
+
 });
+
+describe("Router", () => {
+
+  it(`should return HTTP 200 when making a POST request to /signup`, () => {
+    assert.equal(true, true)
+  });
+  
+  it(`should return HTTP 200 when making a GET request to /signin`, () => {
+    assert.equal(true, true)
+  });
+  
+  it(`should not allow PUT, DELETE, PATCH requests to any endpoint`, () => {
+    assert.equal(true, true)
+  });
+
+});
+
+describe("Body Parser"), () => {
+
+  it(`should not receive undefined when there is no body`, () =>{
+    assert.equal(true, true)
+  });
+
+  it(`should receive a body as an object`, () => {
+    assert.equal(true, true)
+  });
+
+}
+
+describe("/signup", () => {
+  
+  it(`must receive user data in the controller`, () => {
+    assert.equal(true, true)
+  });
+
+  it(`should store user in database`, () => {
+    assert.equal(true, true)
+  });
+  
+  it(`returns a confirmation that the user was created when username, password, e-mail, full name, age and address are provided correctly`, () => {
+    assert.equal(true, true)
+  })
+  
+  it('should migrate the user to the database', () => {
+
+  });
+
+})
